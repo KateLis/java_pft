1cbfe0afc392c164b8da009472116c817f84d6d7
KateLis <qualitysw100@gmail.com>
2018-02-08 19:16:28 +0300

    After math functions

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,6 +1,11 @@
+package ru.stqa.pft.sandbox;
+
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
+
 		System.out.println("Hello, world!");
+
+        System.out.println(1.0/2);
 	}
 }
