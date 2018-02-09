3f3641c6f7c227962fb233abac80ac2c8be96191
KateLis <qualitysw100@gmail.com>
2018-02-09 14:34:47 +0300

    I've added several functions

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,9 +3,25 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
+       hello("Kate");
 
-		System.out.println("Hello, world!");
+       double len = 6;
+       System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
 
-        System.out.println(1.0/2);
-	}
+       double ava = 5;
+       double beta = 7;
+       System.out.println("Площадь прямоугольника со сторонами " + ava + " и " + beta + " = " + area(ava, beta));
+    }
+
+	public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static double area(double l){
+	    return l*l;
+    }
+
+    public static double area(double a, double b) {
+	    return a * b;
+    }
 }
