582a9883ccc4f6e10d03fd4c3a0ac7473022a5d2
KateLis <qualitysw100@gmail.com>
2018-02-09 18:48:42 +0300

    Added constructor function that allows to fill parametrs inside new Object()

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,26 +2,26 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 
-	public static void main(String[] args) {
-       hello("Kate");
+    public static void main(String[] args) {
+        hello("Kate");
 
-       double len = 6;
-       System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
 
-       double ava = 5;
-       double beta = 7;
-       System.out.println("Площадь прямоугольника со сторонами " + ava + " и " + beta + " = " + area(ava, beta));
+        Rectangle r = new Rectangle(8, 9);
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
     }
 
-	public static void hello(String somebody) {
+    public static void hello(String somebody) {
         System.out.println("Hello, " + somebody + "!");
     }
 
-    public static double area(double l){
-	    return l*l;
+    public static double area(Square s){
+        return s.l*s.l;
     }
 
-    public static double area(double a, double b) {
-	    return a * b;
+    public static double area(Rectangle r) {
+        return r.a * r.b;
     }
 }
+
